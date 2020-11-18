421904341a2ae979e265e0168489ddedeb78ca57
Crowdbotics <team@crowdbotics.com>
2020-11-18 19:59:19 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/birdo_network_22735/MainActivity.java
@@ -0,0 +1,15 @@
+package com.birdo_network_22735;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "birdo_network_22735";
+    }
+}
